0a27c7340def1b25c50e705cce6838412a7ac83d
Léo Gato <leo.costa@tegra.me>
2018-01-07 01:44:54 -0200

    Java 8 on

--- src/main/java/br/com/lelo/precojsf/oo/especificacao/Gratificador.java
+++
@@ -1,9 +0,0 @@
-package br.com.lelo.precojsf.oo.especificacao;
-
-import br.com.lelo.precojsf.oo.bean.Funcionario;
-
-public interface Gratificador {
-	
-	public double calcularGratificacao(Funcionario funcionario);
-
-}
